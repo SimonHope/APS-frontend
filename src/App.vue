59ca81abbc0c0f83cb789d5b5deedbfa5ec863ef
Chayanin Buasala <chayanin.gamer@gmail.com>
2021-10-13 16:44:20 +0700

    Reset Project 2nd

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Navbar   ></Navbar>
+    <!-- <NavbarStu  ></NavbarStu> -->
+    <router-view></router-view>
+    <!-- <footer><footers></footers></footer> -->
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/NavbarGen.vue";
+// import NavbarStu from "./components/NavbarStudent.vue"
+// import footers from "./components/footers.vue";
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    // NavbarStu,
+    // footers,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 3% 0% 0% 0%;
+  background-color: rgb(255, 255, 255);
+  text-align: center;
+  background-image: url("./assets/background.png");
+  font-family: "Prompt", sans-serif;
+}
+@import url("https://fonts.googleapis.com/css2?family=Prompt&display=swap");
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <Homepage />
+</template>
+
+<script>
+  import Homepage from '../components/Homepage.vue'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      Homepage,
+    },
+  }
+</script>
